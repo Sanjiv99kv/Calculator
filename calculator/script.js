8bf35a9a82532cc2c39de70f55b0dea6872d984e
Sanjiv99kv <143729099+Sanjiv99kv@users.noreply.github.com>
2023-09-19 11:57:57 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,25 @@
+let str="";
+let input = document.getElementById("input-box");
+let button=document.querySelectorAll("button");
+let arr=Array.from(button);
+arr.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        if(e.target.innerHTML=="AC"){
+            str="";
+            input.value=str;
+        }
+        else if(e.target.innerHTML=="DEL"){
+            str=str.slice(0,str.length-1);
+            input.value=str;
+        }
+        else if(e.target.innerHTML=="="){
+            str=eval(str);
+            input.value=str;
+        }
+        else{
+            str+=e.target.innerHTML;
+            input.value=str;
+        }
+        
+    })    
+});
